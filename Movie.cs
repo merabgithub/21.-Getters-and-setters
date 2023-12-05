b69ce7ac3ce8d4c2dfd894661c69fe99b2e1e67e
MercyM <MercyM@S-MERCYM-Z15UG6>
2023-12-05 20:53:14 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_setters
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; //now can only be set by getters and setters, for
+
+        //constructor
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; // now only using the getters and setters values
+        }
+        public string Rating
+        {
+            get { return rating; }
+
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");//new
+            Movie movie2 = new Movie("Unreleased", "Mercy", "PG");
+
+            Console.WriteLine(movie1.Rating);
+            Console.ReadLine();
+        }
+    }
+}
